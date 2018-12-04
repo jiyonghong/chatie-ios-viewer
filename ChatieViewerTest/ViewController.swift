5963f787c5da5bdef394685f4516b654a0f37dee
Jiyong Hong <jiyongh0204@gmail.com>
2018-12-04 18:54:45 +0900

    Initial Commit

---
+++ ChatieViewerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatieViewerTest
+//
+//  Created by redice on 04/12/2018.
+//  Copyright © 2018 Jiyong Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
